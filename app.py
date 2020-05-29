1488bed5d720ed3007ba0f456934f3954ed28d70
LeoKnox <LeoKnox@users.noreply.github.com>
2020-05-28 20:47:55 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -9,6 +9,50 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'pl
 
 db = SQLAlchemy(app)
 
+@app.cli.command('db_create')
+def db_create():
+    db.create_all()
+    print('Database created')
+
+@app.cli.command('db_drop')
+def db_drop():
+    db.drop_all()
+    print('Database dropped')
+
+@app.cli.command('db_seed')
+def db_seed():
+    mercury = Planet(   planet_name='Mercury',
+                        planet_type='Class D',
+                        home_star='Sol',
+                        mass=3.258e23,
+                        radius=1516,
+                        distance=35.98e6)
+
+    venus = Planet(   planet_name='Venus',
+                        planet_type='Class K',
+                        home_star='Sol',
+                        mass=4.867e24,
+                        radius=3760,
+                        distance=67.24e6)
+
+    earth = Planet(   planet_name='Earth',
+                        planet_type='Class M',
+                        home_star='Sol',
+                        mass=5972e24,
+                        radius=3959,
+                        distance=92.96e6)
+    
+    db.session.add(mercury)
+    db.session.add(venus)
+    db.session.add(earth)
+
+    test_user = User(   first_name='Test',
+                        last_name='User',
+                        email='test@user.com',
+                        password='1234')
+    
+    db.session.add(test_user)
+
 @app.route('/')
 def hello_world():
     return jsonify(message='Start programming'), 200
@@ -50,6 +94,6 @@ class Planet(db.Model):
     mass = Column(Float)
     radius = Column(Float)
     distance = Column(Float)
-    
+
 if __name__ == '__main__':
     app.run()
